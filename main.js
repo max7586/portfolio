bceb420932a6f7026fc76578f0dea7487b90844e
Ahmed <ahmed.max7586@gmail.com>
2019-11-20 06:30:27 -0500

    header mobile first responsive

--- main.js
+++ main.js
@@ -1,27 +1,37 @@
 "use strict";
 $(document).ready(function() {
-  var bottom = $("header").outerHeight();
-  var header = $("header").offset().top + bottom;
-  $(window).scroll(function() {
-    // header fixed only when window width greater than 991px
-    if ($(window).width() > 991) {
-      if ($(window).scrollTop() >= header) {
-        $("header").addClass("sticky");
-      } else {
-        $("header").removeClass("sticky");
-      }
-    }
-  });
+  // var bottom = $("header").outerHeight();
+  // var header = $("header").offset().top + bottom;
+  // $(window).scroll(function() {
+  //   // header fixed only when window width greater than 991px
+  //   if ($(window).width() > 991) {
+  //     if ($(window).scrollTop() >= header) {
+  //       $("header").addClass("sticky");
+  //     } else {
+  //       $("header").removeClass("sticky");
+  //     }
+  //   }
+  // });
 
   // navbar active link
 
-  $(".navbar ul li a").click(function() {
-    $(".navbar ul li a").removeClass("active");
-    $(this).addClass("active");
+  // $(".navbar ul li a").click(function() {
+  //   $(".navbar ul li a").removeClass("active");
+  //   $(this).addClass("active");
+  // });
+
+  // $(".ham-burger").click(function() {
+  //   $(this).toggleClass("active");
+  //   $(".navbar").toggleClass("open");
+  // });
+
+  $(".menu").on("click", function() {
+    $(this).toggleClass("active");
+    $(".overlay").toggleClass("menu-open");
   });
 
-  $(".ham-burger").click(function() {
-    $(this).toggleClass("active");
-    $(".navbar").toggleClass("open");
+  $(".nav a").on("click", function() {
+    $(".menu").removeClass("active");
+    $(".overlay").removeClass("menu-open");
   });
 });
